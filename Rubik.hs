ea670eb17db046f2fc86c6a83e71a3c1f33fcf64
Matthew Hall <hallmg1@student.op.ac.nz>
2020-10-13 20:30:28 +1300

    Why

--- Rubik.hs
+++ Rubik.hs
@@ -1,5 +1,6 @@
 module Rubik where
 
+import qualified Data.Map.Strict as M
 import Text.Printf
 
 data Color = Wh | Gr | Re | Bl | Or | Ye deriving (Show, Eq)
@@ -11,34 +12,28 @@ instance PrintfArg Color where
     formatString (show c) (fmt { fmtChar = 's', fmtPrecision = Nothing})
   formatArg _ fmt = errorBadFormat $ fmtChar fmt
 
--- Each field is a different sticker/square.
--- The numbers correspond to the following squares on a face:
--- 1 2 3
--- 8 x 4
--- 7 6 5
-data Face = Face { s1 :: Color
-                 , s2 :: Color
-                 , s3 :: Color
-                 , s4 :: Color
-                 , s5 :: Color
-                 , s6 :: Color
-                 , s7 :: Color
-                 , s8 :: Color
-                 } deriving (Show)
-
--- Squares can also be accessed through these functions:
+-- Each symbol is a different sticker/square.
 -- C = corner, E = edge
-faceCA = s1
-faceEA = s2
-faceCB = s3
-faceEB = s4
-faceCC = s5
-faceEC = s6
-faceCD = s7
-faceED = s8
+-- The symbols correspond to the following squares on a face:
+-- CA EA CB
+-- ED -- EB
+-- CD EC CC
+data Square = CA | EA | CB | EB | CC | EC | CD | ED
+  deriving (Show, Eq, Ord, Enum)
 
+-- A face is a map from a Square to a Color
+type Face = M.Map Square Color
+
+-- Face objects should be instantiated with this
+-- to ensure all key slots are filled.
 plainFace :: Color -> Face
-plainFace c = Face c c c c c c c c
+plainFace = faceFromList . replicate 8
+
+-- Testing function
+faceFromList :: [Color] -> Face
+faceFromList cs
+  | length cs == 8 = M.fromList $ zip [CA .. ED] cs
+  | otherwise      = error "Face objects require exactly 8 Colors."
 
 data Cube = Cube { up    :: Face
                  , front :: Face
@@ -57,64 +52,218 @@ baseCube = Cube { up    = plainFace Wh
                 , down  = plainFace Ye
                 }
 
+-- Variants will be added after basic turn function's implementation.
+-- Rotations/slices will be added at a later date.
+data Move = U | U2 | U'
+          | F | F2 | F'
+          | R | R2 | R'
+          | B | B2 | B'
+          | L | L2 | L'
+          | D | D2 | D'
+
+-- Effects of one clockwise rotation:
+
+-- Main face squares rotate 90 degrees:
+
+-- This    becomes   this
+-- CA EA CB          CD ED CA
+-- ED    EB          EC    EA
+-- CD EC CC          CC EB CB
+baseRotateFace :: Face -> Face
+baseRotateFace f = faceFromList cs'
+  where
+    cs  = M.elems f
+    cs' = drop 6 cs ++ take 6 cs
+
+-- Side squares rotate as follows:
+
+-- U
+
+-- F  -> L  -> B  -> R  -> F
+-- CA    CA    CA    CA
+-- EA    EA    EA    EA
+-- CB    CB    CB    CB
+
+-- D
+-- F  -> R  -> B  -> L  -> F
+-- CD    CD    CD    CD
+-- EC    EC    EC    EC
+-- CC    CC    CC    CC
+
+-- F
+-- U  -> R  -> D  -> L  -> U
+-- CD    CA    CB    CC
+-- EC    ED    EA    EB
+-- CC    CD    CA    CB
+
+-- B
+-- U  -> L  -> D  -> R  -> U
+-- CB    CA    CD    CC
+-- EA    ED    EC    EB
+-- CA    CD    CC    CB
+
+-- R
+-- U  -> B  -> D  -> F  -> U
+-- CC    CA    CC    CC
+-- EB    ED    EB    EB
+-- CB    CD    CB    CB
+
+-- L
+-- U  -> F  -> D  -> B  -> U
+-- CA    CA    CA    CC
+-- ED    ED    ED    EB
+-- CD    CD    CD    CB
+
+-- More generally,
+
+-- S
+-- Na -> Nb -> Nc -> Nd -> Na
+-- a1    b1    c1    d1
+-- a2    b2    c2    d2
+-- a3    b3    c3    d3
+
+type SideSpecs = (RSpec,RSpec,RSpec,RSpec)
+type RSpec = (Face,[Square])
+type SCPair = (Square,Color)
+
+baseRotateSides :: SideSpecs -> (Face,Face,Face,Face)
+baseRotateSides ((a,[]),(b,[]),(c,[]),(d,[])) = (a,b,c,d)
+baseRotateSides ( (a, (as:ar))
+                , (b, (bs:br))
+                , (c, (cs:cr))
+                , (d, (ds:dr))
+                ) = baseRotateSides ((a',ar),(b',br),(c',cr),(d',dr))
+  where
+    cFromS :: Square -> Face -> Color
+    cFromS s m = case M.lookup s m of
+      (Just c) -> c
+      Nothing  -> error ("Missing square" ++ show s)
+    a' = M.insert as (cFromS ds d) a
+    b' = M.insert bs (cFromS as a) b
+    c' = M.insert cs (cFromS bs b) c
+    d' = M.insert ds (cFromS cs c) d
+
+baseRotateRow :: (SCPair,SCPair,SCPair,SCPair) -> (SCPair,SCPair,SCPair,SCPair)
+baseRotateRow ((sa,a),(sb,b),(sc,c),(sd,d)) = ((sa,d),(sb,a),(sc,b),(sd,c))
+
+baseRotate :: Face -> SideSpecs -> (Face,Face,Face,Face,Face)
+baseRotate f s = (f', a', b', c', d')
+  where
+    f' = baseRotateFace f
+    (a',b',c',d') = baseRotateSides s
+
+baseRotateU :: Cube -> Cube
+baseRotateU c = c {up=u',front=f',left=l',back=b',right=r'}
+  where
+    (u',f',l',b',r') = baseRotate (up c) ( (front c,[CA,EA,CB])
+                                         , (left  c,[CA,EA,CB])
+                                         , (back  c,[CA,EA,CB])
+                                         , (right c,[CA,EA,CB])
+                                         )
+
+baseRotateD :: Cube -> Cube
+baseRotateD c = c {down=d',front=f',right=r',back=b',left=l'}
+  where
+    (d',f',r',b',l') = baseRotate (down c) ( (front c,[CD,EC,CC])
+                                           , (right c,[CD,EC,CC])
+                                           , (back  c,[CD,EC,CC])
+                                           , (left  c,[CD,EC,CC])
+                                           )
+
+baseRotateF :: Cube -> Cube
+baseRotateF c = c {front=f',up=u',right=r',down=d',left=l'}
+  where
+    (f',u',r',d',l') = baseRotate (front c) ( (up    c,[CD,EC,CC])
+                                            , (right c,[CA,ED,CD])
+                                            , (down  c,[CB,EA,CA])
+                                            , (left  c,[CC,EB,CB])
+                                            )
+
+baseRotateB :: Cube -> Cube
+baseRotateB c = c {back=b',up=u',left=l',down=d',right=r'}
+  where
+    (b',u',l',d',r') = baseRotate (back c) ( (up    c,[CB,EA,CA])
+                                           , (left  c,[CA,ED,CD])
+                                           , (down  c,[CD,EC,CC])
+                                           , (right c,[CC,EB,CB])
+                                           )
+
+baseRotateR :: Cube -> Cube
+baseRotateR c = c {right=r',up=u',back=b',down=d',front=f'}
+  where
+    (r',u',b',d',f') = baseRotate (right c) ( (up    c,[CC,EB,CB])
+                                            , (back  c,[CA,ED,CD])
+                                            , (down  c,[CC,EB,CB])
+                                            , (front c,[CC,EB,CB])
+                                            )
+
+baseRotateL :: Cube -> Cube
+baseRotateL c = c {left=l',up=u',front=f',down=d',back=b'}
+  where
+    (l',u',f',d',b') = baseRotate (left c) ( (up    c,[CA,ED,CD])
+                                           , (front c,[CA,ED,CD])
+                                           , (down  c,[CA,ED,CD])
+                                           , (back  c,[CC,EB,CB])
+                                           )
+
 -- TODO:
 -- There must be a better way to do this.
 prettyPrintCube :: PrintfType r => Cube -> r
 prettyPrintCube Cube {up=u, front=f, right=r, back=b, left=l, down=d} =
   printf template
     -- Up face
-    (s1 u)
-    (s2 u)
-    (s3 u)
-    (s8 u)
-    (s4 u)
-    (s7 u)
-    (s6 u)
-    (s5 u)
+    u1
+    u2
+    u3
+    u8
+    u4
+    u7
+    u6
+    u5
     -- First layer of middle faces (Left, Front, Right, Back)
-    (s1 l)
-    (s2 l)
-    (s3 l)
-    (s1 f)
-    (s2 f)
-    (s3 f)
-    (s1 r)
-    (s2 r)
-    (s3 r)
-    (s1 b)
-    (s2 b)
-    (s3 b)
+    l1
+    l2
+    l3
+    f1
+    f2
+    f3
+    r1
+    r2
+    r3
+    b1
+    b2
+    b3
     -- Second layer of middle faces
-    (s8 l)
-    (s4 l)
-    (s8 f)
-    (s4 f)
-    (s8 r)
-    (s4 r)
-    (s8 b)
-    (s4 b)
+    l8
+    l4
+    f8
+    f4
+    r8
+    r4
+    b8
+    b4
     -- Third Layer of middle faces
-    (s7 l)
-    (s6 l)
-    (s5 l)
-    (s7 f)
-    (s6 f)
-    (s5 f)
-    (s7 r)
-    (s6 r)
-    (s5 r)
-    (s7 b)
-    (s6 b)
-    (s5 b)
+    l7
+    l6
+    l5
+    f7
+    f6
+    f5
+    r7
+    r6
+    r5
+    b7
+    b6
+    b5
     -- Down face
-    (s1 d)
-    (s2 d)
-    (s3 d)
-    (s8 d)
-    (s4 d)
-    (s7 d)
-    (s6 d)
-    (s5 d)
+    d1
+    d2
+    d3
+    d8
+    d4
+    d7
+    d6
+    d5
   where
     template = unlines
             [ "       %s%s%s"
@@ -127,4 +276,9 @@ prettyPrintCube Cube {up=u, front=f, right=r, back=b, left=l, down=d} =
             , "       %s--%s"
             , "       %s%s%s"
             ]
-
+    [u1,u2,u3,u4,u5,u6,u7,u8] = M.elems u
+    [f1,f2,f3,f4,f5,f6,f7,f8] = M.elems f
+    [r1,r2,r3,r4,r5,r6,r7,r8] = M.elems r
+    [b1,b2,b3,b4,b5,b6,b7,b8] = M.elems b
+    [l1,l2,l3,l4,l5,l6,l7,l8] = M.elems l
+    [d1,d2,d3,d4,d5,d6,d7,d8] = M.elems d
