33177b216e78d55d0fd8bb48a4963dc1d3cb5687
Matthew Hall <hallmg1@student.op.ac.nz>
2020-10-17 14:21:44 +1300

    Redefined the Move type

--- src/Rubik/Types.hs
+++ src/Rubik/Types.hs
@@ -3,6 +3,8 @@ module Rubik.Types
 , Square(..)
 , Face
 , Cube(..)
+, MoveAmount(..)
+, MoveFace(..)
 , Move(..)
 , Rotation
 , Algorithm
@@ -12,12 +14,15 @@ module Rubik.Types
 , setColor
 , faceColors
 , baseCube
+, turnTimes
 , isNormal
 , isRotation
+, isCombined
 , parseAlgorithm
 ) where
 
 import qualified Data.Map.Strict as M
+import Text.Read (readMaybe)
 import Text.Printf ( PrintfArg
                    , formatArg
                    , fmtChar
@@ -86,26 +91,64 @@ baseCube = Cube { up    = plainFace Wh
                 , down  = plainFace Ye
                 }
 
-          -- Normal moves
-data Move = U | U2 | U'
-          | F | F2 | F'
-          | R | R2 | R'
-          | B | B2 | B'
-          | L | L2 | L'
-          | D | D2 | D'
-          -- Rotations (Translators of moves)
-          | X | X2 | X'
-          | Y | Y2 | Y'
-          | Z | Z2 | Z'
-          deriving (Eq, Ord, Show, Read)
+-- Normal turns, cube rotations
+-- and slices, which are a combination of the previous two.
+data MoveFace = R | L | U | D | F | B
+              | X | Y | Z
+              | M | E | S
+              deriving (Show, Read)
+
+-- Normal turn, double turn, inverse turn.
+data MoveAmount = A1 | A2 | A3
+
+instance Show MoveAmount where
+  show A1 = ""
+  show A2 = "2"
+  show A3 = "'" 
+
+turnTimes :: MoveAmount -> Int
+turnTimes A1 = 1
+turnTimes A2 = 2
+turnTimes A3 = 3
+
+data Move = Move MoveFace MoveAmount
+
+instance Show Move where
+  show (Move f a) = show f ++ show a
+
+-- REVIEW:
+-- Probably a nicer way to do this but it should be okay for now.
+instance Read Move where
+  readsPrec _ s = p $ trim s
+    where
+      trim = dropWhile (==' ')
+      p []          = []
+      p (x:'2':xs)  = [(Move (read [x]) A2, xs)]
+      p (x:'\'':xs) = [(Move (read [x]) A3, xs)]
+      p (x:xs)      = [(Move (read [x]) A1, xs)]
 
 type Rotation = Move
 
 isNormal :: Move -> Bool
-isNormal m = elem m [U,U2,U',F,F2,F',R,R2,R',B,B2,B',L,L2,L',D,D2,D']
+isNormal (Move U _) = True
+isNormal (Move D _) = True
+isNormal (Move R _) = True
+isNormal (Move L _) = True
+isNormal (Move F _) = True
+isNormal (Move B _) = True
+isNormal _          = False
 
 isRotation :: Move -> Bool
-isRotation m = elem m [X,X2,X',Y,Y2,Y',Z,Z2,Z']
+isRotation (Move X _) = True
+isRotation (Move Y _) = True
+isRotation (Move Z _) = True
+isRotation _          = False
+
+isCombined :: Move -> Bool
+isCombined (Move M _) = True
+isCombined (Move E _) = True
+isCombined (Move S _) = True
+isCombined _          = False
 
 -- An algorithm is just a sequence of moves.
 type Algorithm = [Move]
